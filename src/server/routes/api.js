f5fe84bf4cf7314a414a262f6f4d7b1d8118e41b
itszover <jose.martinho@outlook.com.br>
2024-08-28 18:57:39 -0300

    chore: Update API routes in api.js

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,7 +1,11 @@
 import express from "express";
+import controller from "../apiController.js";
 
 let router = express.Router();
 
-router.get("/", function (req, res) { res.send("Hello World pt 3"); });
+router.get("/", controller.index);
+router.get("/select", controller.select);
+router.post("/insert", controller.insert);
+router.post("/login", controller.login);
 
 export default router;
