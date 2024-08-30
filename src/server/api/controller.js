27befee9500cc1d1ac60aa75d579ba8ceb791815
itszover <jose.martinho@outlook.com.br>
2024-08-30 16:45:17 -0300

    feat: Implement user authentication with login functionality

--- src/server/api/controller.js
+++ src/server/api/controller.js
@@ -1,11 +1,44 @@
 import card from "./card.js";
+import User from "./user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 function index(req, res) {
+    let user = new User({ username: "teste", password: "teste" });
+    user.save()
+    
     res.send("Olá");
+
 }
 
-function login(req, res) {
-    res.send("Hello World pt 3");    
+async function login(req, res) {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).send({ message: "Dados inválidos" });
+        return;
+    }
+
+    try {
+        const user = await User.findOne({ username });
+        
+        if (!user) {
+            res.status(401).send({ message: "Usuário não encontrado" });
+            return;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).send({ message: "Senha inválida" });
+            return;
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        res.status(200).send({ token });        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: "Erro ao fazer login" });
+    }   
 }
 
 function insert(req, res) {
