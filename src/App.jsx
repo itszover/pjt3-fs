ea20e3f2ed2e9268427eb837658e07446362ad94
itszover <jose.martinho@outlook.com.br>
2024-08-30 16:44:41 -0300

    chore: Update App.jsx to include search functionality and display search results

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
 import { useState } from 'react';
+import SearchResult from './SearchResult';
+import Input from './Input';
+import Login from './Login';
 
 function App() {
-  let [name, setName] = useState("");
-  let [description, setDescription] = useState("");
-  let [image, setImage] = useState("");
-  let [searchResults, setSearchResults] = useState([]);
+    let [searchResults, setSearchResults] = useState([]);
+    let [isLoggedIn, setIsLoggedIn] = useState(false);
+    let [token, setToken] = useState(null); // State to store the token
 
-  function insertCard() {
-    fetch('http://localhost:3000/api/insert', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, description, image: "https://via.placeholder.com/150" })
-    });
-  }
-
-  async function searchCards() {
-    let response = await fetch(`http://localhost:3000/api/select?name=${name}`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (!response.ok) {
-      throw new Error('Erro ao buscar cartas');
+    function insertCard(card) {
+        fetch('http://localhost:3000/api/insert', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+             },
+            body: JSON.stringify(card)
+        });
     }
-    
-    let data = await response.json();
-    setSearchResults(data);
-  }
 
-  return (
-    <>
-      <div className="card">
-        <input type="text" placeholder='Pessoas' value={name} onChange={(e) => setName(e.target.value)}/>
-        <input type="text" placeholder='Desc' value={description} onChange={(e) => setDescription(e.target.value)}/>
-        <input type="text" placeholder='url' value={image} onChange={(e) => setImage(e.target.value)}/>
-        <button onClick={insertCard}>Inserir Carta</button>
-        <button onClick={searchCards}>Pesquisar</button>
-      </div>
-      <div className="search-results">
-        <ul>
-          {searchResults.map((result, index) => (
-            <li key={index}>{result.name}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+    async function searchCards(name) {
+        let response = await fetch(`http://localhost:3000/api/select?name=${name}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+             }
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro ao buscar cartas');
+        }
+
+        let data = await response.json();
+        setSearchResults(data);
+    }
+
+    const handleLogin = (token) => {
+        setToken(token);
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <div className="container">
+      {!isLoggedIn ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <>
+          <Input onInsertCard={insertCard} />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => searchCards(e.target.value)}
+          />
+          <SearchResult results={searchResults} />
+        </>
+      )}
+    </div>
+    );
 }
 
 export default App;
