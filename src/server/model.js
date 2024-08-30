7d9f086fd28a4de4022d34dd61b35c9482438ad6
itszover <jose.martinho@outlook.com.br>
2024-08-29 21:03:13 -0300

    Remove unused mongoose model and API routes

--- src/server/model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const cards = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: String, required: true }
-});
-
-export default mongoose.model('card', cards);
