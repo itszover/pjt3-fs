3a11b2daa716613523195e712d05c71330447de2
itszover <jose.martinho@outlook.com.br>
2024-08-29 21:01:47 -0300

    chore: Update API routes in app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors"
-import api from "./routes/api.js";
+import api from "./api.js";
 import 'dotenv/config';
 
 let app = express();
 
 app.use(cors())
+app.use(express.json());
 
 app.use("/api", api);
 
