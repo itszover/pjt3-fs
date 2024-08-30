ca4f088618a77c0a9fa7489c865f0d23e56e0dbb
itszover <jose.martinho@outlook.com.br>
2024-08-30 16:45:21 -0300

    chore: Update API routes to include authentication middleware

--- src/server/api/route.js
+++ src/server/api/route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import controller from "./controller.js";
+import auth from "../auth.js";''
 
 let router = express.Router();
 
 router.get("/", controller.index);
-router.get("/select", controller.select);
-router.post("/insert", controller.insert);
+router.get("/select", auth, controller.select);
+router.post("/insert", auth, controller.insert);
 router.post("/login", controller.login);
 
 export default router;
