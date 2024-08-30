1fbeb79dc0cea6e9fc032793f797e31a457ade68
itszover <jose.martinho@outlook.com.br>
2024-08-29 21:02:37 -0300

    chore: Update server index.js to remove unnecessary console.log statement

--- src/server/index.js
+++ src/server/index.js
@@ -5,4 +5,4 @@ mongoose.connect(process.env.DATABASE_URI);
 mongoose.connection.on('error', () => console.log('Error connecting to database'));
 mongoose.connection.once('open', () => console.log('Connected to database'));
 
-app.listen(process.env.PORT, () => {console.log("Server is running on port 3000")});
+app.listen(process.env.PORT, () => console.log("Server is running on port 3000"));
