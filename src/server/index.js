6d4391bdbb16e656cb11353efca5dd3e9382ab10
itszover <jose.martinho@outlook.com.br>
2024-09-01 16:48:27 -0300

    refactor: Update server to use HTTPS for improved security

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,7 @@
 import app from "./app.js";
 import https from "https";
-import mongoose from "mongoose";
+import db from "./config/database.js";
 
-mongoose.connect(process.env.DATABASE_URI);
-mongoose.connection.on('error', () => console.log('Error connecting to database'));
-mongoose.connection.once('open', () => console.log('Connected to database'));
-
-https.createServer(app.listen(process.env.PORT, () => console.log("Server is running on port 3000")))
+db.once('open', () => {
+    https.createServer(app.listen(process.env.PORT, () => console.log("Server is running on port 3000")))
+});
