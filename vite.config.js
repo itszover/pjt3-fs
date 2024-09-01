956f44a60e34af9f4d7cc0a6a5f1b76bb8f2c570
itszover <jose.martinho@outlook.com.br>
2024-09-01 16:47:40 -0300

    feat: Add vite-plugin-compression for improved performance

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        compression()
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom']
+                }
+            }
+        }
+    }
 })
