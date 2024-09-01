9f6a9035bf51ac7d8bfd4c8df279695b8496d83b
itszover <jose.martinho@outlook.com.br>
2024-09-01 17:30:28 -0300

    refactor: Update App component to use useAuth hook and separate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,21 @@
-import { useState } from 'react';
-import SearchResult from './SearchResult';
-import Input from './Input';
-import Login from './Login';
+import useAuth from './hooks/useAuth';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
-    let [searchResults, setSearchResults] = useState([]);
-    let [isLoggedIn, setIsLoggedIn] = useState(false);
-    let [token, setToken] = useState(null); // State to store the token
-    let [searchQuery, setSearchQuery] = useState("");
+    const { isLoggedIn, token, login, logout } = useAuth();
 
-    function insertCard(card) {
-        fetch('http://localhost:3000/api/insert', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(card)
-        });
-    }
-
-    async function searchCards() {
-        let response = await fetch(`http://localhost:3000/api/select?name=${searchQuery}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Erro ao buscar cartas');
+    function renderContent() {
+        if (!isLoggedIn) {
+            return (<Login onLogin={login} />);
+        } else {
+            return (<Home token={token} onLogout={logout} />);
         }
-
-        let data = await response.json();
-        setSearchResults(data);
     }
-
-    const handleLogin = (token) => {
-        setToken(token);
-        setIsLoggedIn(true);
-    };
 
     return (
         <div className="container">
-            {!isLoggedIn ? (
-                <Login onLogin={handleLogin} />
-            ) : (
-                <>
-                    <Input onInsertCard={insertCard} />
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                    <button onClick={searchCards}>Search</button>
-                    <SearchResult results={searchResults} />
-                </>
-            )}
+            {renderContent()}
         </div>
     );
 }
