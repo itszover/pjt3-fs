6bf6afece4de5132817f1c42a2c116a9e2aaaed6
itszover <jose.martinho@outlook.com.br>
2024-09-01 16:48:11 -0300

    refactor: Optimize server app.js with rate limiting and compression middleware

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors"
 import api from "./api/route.js";
+import rateLimit from "express-rate-limit";
+import compression from "compression";
 import 'dotenv/config';
 
 let app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(compression());
 
 app.use("/api", api);
 
