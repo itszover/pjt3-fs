e72bc5bc1eb9bc5566476baa996592bb6eb770f2
itszover <jose.martinho@outlook.com.br>
2024-09-08 18:12:14 -0300

    refactor: Update auth middleware to include logging with winston

--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -1,9 +1,11 @@
 import jwt from 'jsonwebtoken';
+import logger from '../config/logger.js';
 
 function auth(req, res, next) {
     const header = req.headers['authorization'];
     
     if (!header) {
+        logger.warn('Access attempt without token');
         res.status(401).send({ message: "Token não fornecido" });
         return;
     }
@@ -11,6 +13,7 @@ function auth(req, res, next) {
     const token = header.split(' ')[1];
 
     if (!token) {
+        logger.warn('Access attempt with malformed token');
         res.status(401).send({ message: "Token não fornecido" });
         return;
     }
@@ -20,8 +23,7 @@ function auth(req, res, next) {
         req.userId = decoded.id;
         next();
     } catch (error) {
-        console.log(error);
-        
+        logger.error(`Invalid token attempt: ${error.message}`);
         res.status(401).send({ message: "Token inválido" });
     }
 }
