2a9ae10b44b3386a59c7e3bdded8e412ee40b892
itszover <jose.martinho@outlook.com.br>
2024-09-08 19:05:49 -0300

    refactor: Add check-token and logout routes to API
    This commit adds two new routes to the API: "/check-token" and "/logout". These routes are protected by authentication middleware and are used for checking the validity of a token and logging out a user, respectively.

--- src/server/api/route.js
+++ src/server/api/route.js
@@ -6,7 +6,9 @@ let router = express.Router();
 
 router.get("/", controller.index);
 router.get("/select", auth, controller.select);
+router.get("/check-token", auth, controller.checkToken);
 router.post("/insert", auth, controller.insert);
 router.post("/login", controller.login);
+router.post("/logout", auth, controller.logout);
 
 export default router;
